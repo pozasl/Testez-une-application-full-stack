f3e947f7bed3ffe365d48b439bb0dbae563d6918
pozasl <loic.pozas@monlaboweb.com>
2024-09-13 10:15:46 +0000

    [MOD] Improved test coverage with sign/unsigned helpers

--- back/src/test/java/com/openclassrooms/starterjwt/security/jwt/JwtUtilsTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/jwt/JwtUtilsTest.java
@@ -1,26 +1,28 @@
 package com.openclassrooms.starterjwt.security.jwt;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
-
-import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtUtilsTest {
@@ -35,42 +37,39 @@ public class JwtUtilsTest {
         // GIVEN
         ReflectionTestUtils.setField(jwtUtils, "jwtSecret", "jwtKey");
         ReflectionTestUtils.setField(jwtUtils, "jwtExpirationMs", 5000);
-
     }
 
     @Nested
     class withAuthTest {
 
-        @BeforeEach
-        void setupAuth() {
+        @Test
+        public void generateJwtToken_shouldGenerateToken() {
+            // GIVEN
             UserDetails useDetails = new UserDetailsImpl(1L, "bob@test.com", "Bob", "Le Bricoleur", true, "pass4321");
             authMock = mock(Authentication.class);
             when(authMock.getPrincipal()).thenReturn(useDetails);
-        }
-
-        @Test
-        public void generateJwtToken_shouldGenerateToken() {
             // WHEN
             String token = jwtUtils.generateJwtToken(authMock);
             // THEN
             System.out.println(token);
-            assertNotNull(token);
+            assertThat(token).isNotNull();
         }
 
         @Test
         public void givenGoodToken_getUserNameFromJwtToken_shouldGetUserNameAsEmail() {
             // GIVEN
-            String token = jwtUtils.generateJwtToken(authMock);
+            String userEmail = "bob@test.com";
+            String token = sign(userEmail, 5000, "jwtKey");
             // WHEN
             String email = jwtUtils.getUserNameFromJwtToken(token);
             // THEN
-            assertEquals("bob@test.com", email);
+            assertThat(email).isEqualTo(userEmail);
         }
 
         @Test
         public void givenGoodToken_validateJwtToken_shouldReturnTrue() {
             // GIVEN
-            String token = jwtUtils.generateJwtToken(authMock);
+            String token = sign("bob@test.com", 5000, "jwtKey");
             // WHEN
             boolean valid = jwtUtils.validateJwtToken(token);
             // THEN
@@ -83,7 +82,7 @@ public class JwtUtilsTest {
     @ExtendWith(OutputCaptureExtension.class)
     public void givenExpiredToken_validateJwtToken_ShouldReturnFalseAndLogIt(CapturedOutput capture) {
         // GIVEN
-        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJib2JAdGVzdC5jb20iLCJpYXQiOjE3MjYxMjUzODIsImV4cCI6MTcyNjEyNTM4NX0.8Dr6F9ZSmR01GH_3nMUM2hqloKKLOJAFF0LoI6a-cITi88jjtF3fOBjVDrZWjVyqzYG9jL-7YVOsSFrAKWAvnw";
+        String token = sign("bob@test.com", 1, "jwtKey");
         // WHEN
         boolean valid = jwtUtils.validateJwtToken(token);
         // THEN
@@ -104,4 +103,73 @@ public class JwtUtilsTest {
         assertThat(capture.getOut()).contains("Invalid JWT token: ");
     }
 
+    @Test
+    @ExtendWith(OutputCaptureExtension.class)
+    public void givenBadSignedToken_validateJwtToken_ShouldReturnFalseAndLogIt(CapturedOutput capture) {
+        // GIVEN
+        String token = sign("bob@test.com", -1, "badJwtKey");
+        // WHEN
+        boolean valid = jwtUtils.validateJwtToken(token);
+        // THEN
+        assertFalse(valid);
+        assertThat(capture.getOut()).contains("Invalid JWT signature: ");
+    }
+
+    @Test
+    @ExtendWith(OutputCaptureExtension.class)
+    public void givenUnsupportedToken_validateJwtToken_ShouldReturnFalseAndLogIt(CapturedOutput capture) {
+        // GIVEN
+        String token = unsigned("bob@test.com", 5000, "jwtKey");
+        // WHEN
+        boolean valid = jwtUtils.validateJwtToken(token);
+        // THEN
+        assertFalse(valid);
+        assertThat(capture.getOut()).contains("JWT token is unsupported: ");
+    }
+
+    @Test
+    @ExtendWith(OutputCaptureExtension.class)
+    public void givenEmptyClaimToken_validateJwtToken_ShouldReturnFalseAndLogIt(CapturedOutput capture)
+            throws Exception {
+        // GIVEN
+        String token = "";
+        // WHEN
+        boolean valid = jwtUtils.validateJwtToken(token);
+        // THEN
+        assertFalse(valid);
+        assertThat(capture.getOut()).contains("JWT claims string is empty: ");
+    }
+
+    /**
+     * Helper to create signed test token
+     *
+     * @param email
+     * @param jwtExpirationMs
+     * @param jwtSecret
+     * @return
+     */
+    private String sign(String email, int jwtExpirationMs, String jwtSecret) {
+        return Jwts.builder()
+                .setSubject((email))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    /**
+     * Helper to create unsigned test token
+     *
+     * @param email
+     * @param jwtExpirationMs
+     * @param jwtSecret
+     * @return
+     */
+    private String unsigned(String email, int jwtExpirationMs, String jwtSecret) {
+        return Jwts.builder()
+                .setSubject((email))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .compact();
+    }
 }
