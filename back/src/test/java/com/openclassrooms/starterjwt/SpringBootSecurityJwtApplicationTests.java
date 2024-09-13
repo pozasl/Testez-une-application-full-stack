6d69b8eb7e9437914d57beb164e8c63839262c33
pozasl <loic.pozas@monlaboweb.com>
2024-09-13 07:31:14 +0000

    [ADD] App initialization UT

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,13 +1,38 @@
 package com.openclassrooms.starterjwt;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.openclassrooms.starterjwt.controllers.AuthController;
+import com.openclassrooms.starterjwt.controllers.SessionController;
+import com.openclassrooms.starterjwt.controllers.TeacherController;
+import com.openclassrooms.starterjwt.controllers.UserController;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private AuthController authController;
+
+  @Autowired
+  private UserController userController;
+
+  @Autowired
+  private TeacherController teacherController;
+
+  @Autowired
+  private SessionController sessionController;
+
+
+  @Test
+  void contextLoads() {
+    assertNotNull(authController);
+    assertNotNull(userController);
+    assertNotNull(teacherController);
+	  assertNotNull(sessionController);
+  }
 
 }
