f9e3d74d25c36db7cdc29402da6848942222bbae
pozasl <loic.pozas@monlaboweb.com>
2024-09-16 13:39:06 +0000

    [MOD] App component IT

--- front/src/app/app.component.test.ts
+++ front/src/app/app.component.test.ts
@@ -40,23 +40,10 @@ describe('AppComponent', () => {
       fixture.detectChanges();
     });
 
-    it('should create the app', () => {
-      expect(app).toBeTruthy();
-    });
-  
-    it('$isLogged should return False as a boolean Observable', (done) => {
-      app.$isLogged().subscribe((logged) => {
-        expect(logged).toBe(false);
-        done();
-      })
-    });
-
-    it('logout should call sessionService logout and go back to site root', () => {
-      const sessionServiceSpy = jest.spyOn(sessionService, 'logOut');
-      const routerSpy = jest.spyOn(router, 'navigate');
-      app.logout();
-      expect(sessionServiceSpy).toBeCalled();
-      expect(routerSpy).toBeCalledWith(['']);
+    it('should display Login and Register links', () => {
+      const links: HTMLElement[] = fixture.nativeElement.querySelectorAll('span.link');
+      expect(links[0].textContent).toBe('Login');
+      expect(links[1].textContent).toBe('Register');
     });
 
   });
@@ -70,17 +57,18 @@ describe('AppComponent', () => {
       fixture.detectChanges();
     });
 
-    it('$isLogged should return True as a boolean Observable', (done) => {
-      app.$isLogged().subscribe((logged) => {
-        expect(logged).toBe(true);
-        done();
-      })
+    it('should display Session, Account and Logout links', () => {
+      const links: HTMLElement[] = fixture.nativeElement.querySelectorAll('span.link');
+      expect(links[0].textContent).toBe('Sessions');
+      expect(links[1].textContent).toBe('Account');
+      expect(links[2].textContent).toBe('Logout');
     });
 
-    it('logout should call sessionService logout and go back to site root', () => {
+    it('Click on logout Link should log user out and go back to site root', () => {
       const sessionServiceSpy = jest.spyOn(sessionService, 'logOut');
       const routerSpy = jest.spyOn(router, 'navigate');
-      app.logout();
+      const logoutLink:HTMLElement = fixture.nativeElement.querySelectorAll('span.link')[2]
+      logoutLink.click();
       expect(sessionServiceSpy).toBeCalled();
       expect(routerSpy).toBeCalledWith(['']);
     });
