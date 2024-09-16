902e5ac295badebf0162d9f761bf4e17338c6a6f
pozasl <loic.pozas@monlaboweb.com>
2024-09-16 06:47:40 +0000

    [MOD] renamed as IT

--- back/src/test/java/com/openclassrooms/starterjwt/security/jwt/AuthEntryPointJwtIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/jwt/AuthEntryPointJwtIT.java
@@ -20,7 +20,7 @@ import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.security.core.AuthenticationException;
 
 @ExtendWith(MockitoExtension.class)
-public class AuthEntryPointJwtTest {
+public class AuthEntryPointJwtIT {
 
     @Mock
     private HttpServletRequest requestMock;
